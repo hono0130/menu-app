4d327ea9c7b6ed64b8d8f19d65e0defac015cd56
ShinyaIshitobi <shinya.ishitobi@akariinc.co.jp>
2022-10-08 15:51:49 +0900

    Init

---
+++ frontend/src/component/Logo.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+import { Box, Text } from '@chakra-ui/react'
+
+const Logo: FC = (props: any) => {
+  return (
+    <Box {...props}>
+      <Text fontSize="lg" fontWeight="bold">
+        Logo
+      </Text>
+    </Box>
+  )
+}
+
+export default Logo
