d9952e25f2062017ddcabd876859fa70dd98331a
ShinyaIshitobi <shinya.ishitobi@akariinc.co.jp>
2022-10-09 19:29:26 +0900

    feat: 繋ぎ込み部分

--- frontend/src/graphql/config.ts
+++ frontend/src/graphql/config.ts
@@ -1,8 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
-import gql from 'graphql-tag'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/',
+  uri: 'http://localhost:8000/graphql',
   cache: new InMemoryCache(),
 })
 
