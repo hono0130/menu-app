8d0b51fa1e0815090258c31bb6116e6159beebef
ShinyaIshitobi <shinya.ishitobi@akariinc.co.jp>
2022-10-09 19:29:57 +0900

    feat: CORS対策

--- backend/app.py
+++ backend/app.py
@@ -3,12 +3,24 @@ from fastapi import FastAPI
 from strawberry.asgi import GraphQL
 from strawberry.scalars import JSON
 from crawling import crawling
+from starlette.middleware.cors import CORSMiddleware
 
 from schema import Menu
 from crawling import crawling
 
+app = FastAPI()
 
+origins = [
+  "http://localhost:3000",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @strawberry.type
 class Query:
@@ -21,7 +33,6 @@ schema = strawberry.Schema(query=Query)
 
 graphql_app = GraphQL(schema)
 
-app = FastAPI()
 app.add_route("/graphql", graphql_app)
 app.add_websocket_route("/graphql", graphql_app)
 
